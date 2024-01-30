8fec6f968ff0e819b0f367f281fab734aef7c8a9
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-01-30 16:54:56 -0500

    Advent of Code: Day 3 Rucksacks: Reorganized data into separate module, renamed functions and tests to prepare for second half implementation

---
+++ Advent of Code/test_rucksack.py
@@ -0,0 +1,17 @@
+import unittest
+from rucksack_data import example_rucksacks, actual_rucksacks
+from rucksack import solve_rucksacks_first_half, solve_rucksacks_second_half
+
+class TestRucksack(unittest.TestCase):
+    def test_first_half_example(self):
+        self.assertEqual(solve_rucksacks_first_half(example_rucksacks), 157)
+        
+    def test_first_half_actual(self):
+        
+        self.assertEqual(solve_rucksacks_first_half(actual_rucksacks), 8039)
+
+    def test_second_half_example(self):
+        self.assertEqual(solve_rucksacks_second_half(example_rucksacks), 70)
+        
+if __name__ == "__main__":
+    unittest.main()
